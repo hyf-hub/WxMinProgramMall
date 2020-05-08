21a5697b91495847855ff378c44135b894c30919
FL <1458174276@qq.com>
2020-05-08 09:34:41 +0800

    通过计算属性更改内容

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -9,17 +9,14 @@ Page({
   behaviors: [computedBehavior],
   data: {
     cartlist: [],
-    isShow:false
   },
   computed: {
-    // isShow(data) {
-    //   let cartlist = data.cartlist;
-    //   let select =  cartlist.find(item=>{
-    //     return item.select
-    //   })
-    //   console.log(Boolean(select));
-    //   return Boolean(select)
-    // },
+    isShow(data) {
+      let cartlist = data.cartlist;
+      let select = cartlist.find(item => item.select)
+      console.log(Boolean(select));
+      return Boolean(select)
+    },
 
     // bottomShow(data) {
     //   let cartlist = data.cartlist;
@@ -41,29 +38,35 @@ Page({
     this.setData({
       cartlist: check,
     })
-    this.changeStatue()
   },
   itemselect(e) {
+    let cartlist = this.data.cartlist
     let isShow = this.data.isShow
     let index = e.detail
-    let cartlist = this.data.cartlist
-    cartlist[index].select = !cartlist[index].select
+    // cartlist[index].select = !cartlist[index].select
+    let temp = cartlist[index]
+    temp.select = !temp.select
+    cartlist =  cartlist.splice(index, 1, temp)
+    //这里直接通过索引更改值不会触发计算属性
+    // const check = cartlist.map(item => {
+    //   item.select = isShow
+    //   return item
+    // })
     // [`cartlist[${index}].select`]: select
     this.setData({
-      cartlist
+      cartlist: cartlist
     })
-    this.changeStatue()
-  },
-  changeStatue() {
-    let cartlist = this.data.cartlist;
-    let select = cartlist.find(item => {
-      return item.select
-    })
-    console.log(Boolean(select));
-    // return Boolean(select)
-    this.setData({
-      isShow: Boolean(select)
-    })
+    //  console.log('全选');
+    // let cartlist = this.data.cartlist
+    // let isShow = this.data.isShow
+    // const check = cartlist.map(item => {
+    //   item.select = !isShow
+    //   return item
+    // })
+    // this.setData({
+    //   cartlist: check,
+    // })
+
   },
   /**
    * 生命周期函数--监听页面加载
